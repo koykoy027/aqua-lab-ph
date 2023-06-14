54c7ed80003132945dc6f49a66f3f3115f6d7fce
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-14 13:37:55 +0800

    hide settings and earnings in profile dropdown

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -28,13 +28,13 @@
                             class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
                     </li>
 
-                    <li>
+                    {{-- <li>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
                     </li>
 
                     <li>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Earnings</a>
-                    </li>
+                    </li> --}}
 
                     <li>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
