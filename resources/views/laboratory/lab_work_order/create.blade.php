4ac05ada8fecd178406682a46f5d00ceb380e5b5
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-14 13:12:24 +0800

    raw data file dynamic depends on test parameters

--- resources/views/laboratory/lab_work_order/create.blade.php
+++ resources/views/laboratory/lab_work_order/create.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Create Raw Data File')
 @section('content')
 
+    {{-- MICRO CONDITION --}}
     @if ($rawDataFile->test_parameters == 'MICR1 - Heterotrophic Plate Count (HPC)')
         @include('laboratory.lab_work_order.partials.micro.micr1')
     @endif
@@ -9,35 +10,77 @@
     @if ($rawDataFile->test_parameters == 'MICR2 - Thermotolerant Colifom Test')
         @include('laboratory.lab_work_order.partials.micro.micr2')
     @endif
+
     @if ($rawDataFile->test_parameters == 'MICR3 - Total Coliform')
         @include('laboratory.lab_work_order.partials.micro.micr3')
     @endif
+
     @if ($rawDataFile->test_parameters == 'MICR4 - E. coli Test')
         @include('laboratory.lab_work_order.partials.micro.micr4')
     @endif
+
     @if ($rawDataFile->test_parameters == 'MICR5 - All three (3) Mandatory Microbiological Parameters (PNSDW 2017/DOH AO 2013-003)')
         @include('laboratory.lab_work_order.partials.micro.micr1')
         @include('laboratory.lab_work_order.partials.micro.micr2')
         @include('laboratory.lab_work_order.partials.micro.micr3')
     @endif
 
+    {{-- CHEM CONDITION--}}
+    @if ($rawDataFile->test_parameters == 'CHEM1 - pH')
+        @include('laboratory.lab_work_order.partials.chem.chem1')
+    @endif
 
+    @if ($rawDataFile->test_parameters == 'CHEM2 - Nitrate')
+        @include('laboratory.lab_work_order.partials.chem.chem2')
+    @endif
 
+    @if ($rawDataFile->test_parameters == 'CHEM3 - Total Dissolved Solids')
+        @include('laboratory.lab_work_order.partials.chem.chem3')
+    @endif
 
-    {{-- @include('laboratory.lab_work_order.partials.chem.chem1')
-    @include('laboratory.lab_work_order.partials.chem.chem2')
-    @include('laboratory.lab_work_order.partials.chem.chem3')
-    @include('laboratory.lab_work_order.partials.chem.chem4')
-    @include('laboratory.lab_work_order.partials.chem.chem5')
-    @include('laboratory.lab_work_order.partials.chem.chem6')
-    @include('laboratory.lab_work_order.partials.chem.chem7')
-    @include('laboratory.lab_work_order.partials.chem.chem8')
-    @include('laboratory.lab_work_order.partials.chem.chem9')
-    @include('laboratory.lab_work_order.partials.chem.chem10')
-    @include('laboratory.lab_work_order.partials.phys.phys1')
-    @include('laboratory.lab_work_order.partials.phys.phys2')
-    @include('laboratory.lab_work_order.partials.phys.phys3')
-    @include('laboratory.lab_work_order.partials.phys.phys4') --}}
+    @if ($rawDataFile->test_parameters == 'CHEM4 - Chlorine (Residual), as free')
+        @include('laboratory.lab_work_order.partials.chem.chem4')
+    @endif
 
+    @if ($rawDataFile->test_parameters == 'CHEM5 - Arsenic')
+        @include('laboratory.lab_work_order.partials.chem.chem5')
+    @endif
+
+    @if ($rawDataFile->test_parameters == 'CHEM6 - Cadmium')
+        @include('laboratory.lab_work_order.partials.chem.chem6')
+    @endif
+
+    @if ($rawDataFile->test_parameters == 'CHEM7 - Lead')
+        @include('laboratory.lab_work_order.partials.chem.chem7')
+    @endif
+
+    {{-- @if ($rawDataFile->test_parameters == 'CHEM8 - All Twenty (20) Mandatory Chemical Parameters (DOH AO 2013-003 Dialysis Water)')
+        @include('laboratory.lab_work_order.partials.chem.chem8')
+    @endif --}}
+
+    @if ($rawDataFile->test_parameters == 'CHEM9 - Iron')
+        @include('laboratory.lab_work_order.partials.chem.chem9')
+    @endif
+
+    @if ($rawDataFile->test_parameters == 'CHEM10 - Manganese')
+        @include('laboratory.lab_work_order.partials.chem.chem10')
+    @endif
+
+    {{-- PHYS CONDITION--}}
+    @if ($rawDataFile->test_parameters == 'PHYS1 - Appearance')
+        @include('laboratory.lab_work_order.partials.phys.phys1')
+    @endif
+
+    @if ($rawDataFile->test_parameters == 'PHYS2 - Odor')
+        @include('laboratory.lab_work_order.partials.phys.phys2')
+    @endif
+
+    @if ($rawDataFile->test_parameters == 'PHYS3 - Color, apparent')
+        @include('laboratory.lab_work_order.partials.phys.phys3')
+    @endif
+
+    @if ($rawDataFile->test_parameters == 'PHYS4 - Turbidity')
+        @include('laboratory.lab_work_order.partials.phys.phys4')
+    @endif
 
 @endsection
