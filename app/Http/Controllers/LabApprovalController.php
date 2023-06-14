33a61ebe5231d004452b92d1e2a25a289f7664c6
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-14 11:36:30 +0800

    change Micro1 into Micro Model

--- app/Http/Controllers/LabApprovalController.php
+++ app/Http/Controllers/LabApprovalController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AnalysisRequest;
-use App\Models\Micro1;
+use App\Models\Micro;
 use Illuminate\Http\Request;
 
 class LabApprovalController extends Controller
@@ -20,7 +20,7 @@ class LabApprovalController extends Controller
     {
         $details = AnalysisRequest::find($analysis_id);
 
-        $lab_approval = Micro1::find($details->analysis_id);
+        $lab_approval = Micro::find($details->analysis_id);
 
         return view('laboratory.lab_approval.details', compact('details' , 'lab_approval'));
     }
