ccb2ae9d7b27365866ab7744dfbccac2283fcc85
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-17 16:46:02 +0800

    alert message for changing user status

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,12 +9,12 @@ class UserController extends Controller
 {
     public function setAsActive(User $user){
         $user->update(['status' => '1']);
-        return redirect()->back();
+        return redirect()->back()->with(['message' => 'User status has been changed to Active']);
     }
 
     public function setAsInactive(User $user)
     {
         $user->update(['status' => '0']);
-        return redirect()->back();
+        return redirect()->back()->with(['message' => 'User status has been changed to Inactive']);
     }
 }
