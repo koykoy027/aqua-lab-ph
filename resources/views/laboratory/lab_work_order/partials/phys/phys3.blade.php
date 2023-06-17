947c13225befd5aa3873a899c4f21061f75278fe
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-17 15:47:11 +0800

    delete comment

--- resources/views/laboratory/lab_work_order/partials/phys/phys3.blade.php
+++ resources/views/laboratory/lab_work_order/partials/phys/phys3.blade.php
@@ -1,7 +1,5 @@
 <form action="{{ route('laboratory.lab-work-order-form.phys3', ['analysis_id' => $requests->analysis_id]) }}" method="POST">
     @csrf
-    {{-- <input id="analysis_id" type="hidden" name="analysis_id" value="{{ $requests->analysis_id }}" required autofocus
-        autocomplete="analysis_id" readonly> --}}
 
     <div class="card mb-3 bg-white">
         <h1 class="mb-3">PHYS3 - Color, apparent</h1>
