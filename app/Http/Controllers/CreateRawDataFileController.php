7ef3910b80a0492ee05408c9b42d9a9d8af7edc6
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-19 14:22:13 +0800

    micro1 default value done

--- app/Http/Controllers/CreateRawDataFileController.php
+++ app/Http/Controllers/CreateRawDataFileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\AnalysisRequest;
 use App\Models\LabAcceptance;
+use App\Models\RawData;
 use Illuminate\Http\Request;
 
 class CreateRawDataFileController extends Controller
@@ -11,6 +12,8 @@ class CreateRawDataFileController extends Controller
     public function create($analysis_id){
         $requests = LabAcceptance::find($analysis_id);
         $rawDataFile = AnalysisRequest::find($requests->analysis_id);
-        return view('laboratory.lab_work_order.create', compact('requests', 'rawDataFile'));
+        $rawDataFileValue = RawData::find($requests->analysis_id);
+
+        return view('laboratory.lab_work_order.create', compact('requests', 'rawDataFile', 'rawDataFileValue'));
     }
 }
--- resources/views/laboratory/lab_work_order/partials/micro/micr1.blade.php
+++ resources/views/laboratory/lab_work_order/partials/micro/micr1.blade.php
@@ -1,13 +1,14 @@
 <form action="{{ route ('laboratory.lab-work-order-form.micro1', ['analysis_id' => $requests->analysis_id]) }}" method="POST">
     @csrf
 
+
     <div class="card mb-3 bg-white">
         <h1 class="mb-3">MICR1 - Heterotrophic Plate Count (HPC)</h1>
         <div class="sm:gid-cols-1 grid gap-4 lg:grid-cols-2">
             <div class="mb-3">
                 <label for="micr1_hpc_plate_a">Plate A Colony Count</label>
                 <input id="micr1_hpc_plate_a" type="number" name="micr1_hpc_plate_a"
-                    value="{{ old('micr1_hpc_plate_a') }}" required autofocus autocomplete="micr1_hpc_plate_a"
+                    value="{{ $rawDataFileValue->micr1_hpc_plate_a }}" required autofocus autocomplete="micr1_hpc_plate_a"
                     onchange="micro1()">
                 @error('micr1_hpc_plate_a')
                     <span class="invalid" role="alert">
@@ -19,7 +20,7 @@
             <div class="mb-3">
                 <label for="micr1_hpc_plate_b">Plate B Colony Count</label>
                 <input id="micr1_hpc_plate_b" type="number" name="micr1_hpc_plate_b"
-                    value="{{ old('micr1_hpc_plate_b') }}" required autofocus autocomplete="micr1_hpc_plate_b"
+                    value="{{ $rawDataFileValue->micr1_hpc_plate_b }}" required autofocus autocomplete="micr1_hpc_plate_b"
                     onchange="micro1()">
                 @error('micr1_hpc_plate_b')
                     <span class="invalid" role="alert">
@@ -31,7 +32,7 @@
             <div class="mb-3">
                 <label for="micr1_hpc_average">Average Colony Count</label>
                 <input id="micr1_hpc_average" type="text" name="micr1_hpc_average"
-                    value="{{ old('micr1_hpc_average') }}" required autofocus autocomplete="micr1_hpc_average" readonly>
+                    value="{{ $rawDataFileValue->micr1_hpc_average }}" required autofocus autocomplete="micr1_hpc_average" readonly>
                 @error('micr1_hpc_average')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -42,7 +43,7 @@
             <div class="mb-3">
                 <label for="micr1_hpc_difference">% Difference</label>
                 <input id="micr1_hpc_difference" type="text" name="micr1_hpc_difference"
-                    value="{{ old('micr1_hpc_difference') }}" required autofocus autocomplete="micr1_hpc_difference"
+                    value="{{ $rawDataFileValue->micr1_hpc_difference }}" required autofocus autocomplete="micr1_hpc_difference"
                     readonly>
                 @error('micr1_hpc_difference')
                     <span class="invalid" role="alert">
@@ -54,7 +55,7 @@
             <div class="mb-3">
                 <label for="micr1_hpc_final_result">Final Result</label>
                 <input id="micr1_hpc_final_result" type="text" name="micr1_hpc_final_result"
-                    value="{{ old('micr1_hpc_final_result') }}" required autofocus autocomplete="micr1_hpc_final_result"
+                    value="{{ $rawDataFileValue->micr1_hpc_final_result }}" required autofocus autocomplete="micr1_hpc_final_result"
                     readonly>
                 @error('micr1_hpc_final_result')
                     <span class="invalid" role="alert">
@@ -66,7 +67,7 @@
             <div class="mb-3">
                 <label for="micr1_hpc_remarks">Remarks</label>
                 <input id="micr1_hpc_remarks" type="text" name="micr1_hpc_remarks"
-                    value="{{ old('micr1_hpc_remarks') }}" required autofocus autocomplete="micr1_hpc_remarks"
+                    value="{{ $rawDataFileValue->micr1_hpc_remarks }}" required autofocus autocomplete="micr1_hpc_remarks"
                     readonly>
                 @error('micr1_hpc_remarks')
                     <span class="invalid" role="alert">
