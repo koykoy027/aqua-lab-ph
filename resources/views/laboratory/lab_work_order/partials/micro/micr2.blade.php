80f86d3b118568485afdd68bfc789b781e47f739
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-19 14:23:46 +0800

    micro 2 done

--- resources/views/laboratory/lab_work_order/partials/micro/micr2.blade.php
+++ resources/views/laboratory/lab_work_order/partials/micro/micr2.blade.php
@@ -6,7 +6,7 @@
         <div class="sm:gid-cols-1 grid gap-4 lg:grid-cols-2">
             <div class="mb-3">
                 <label for="micr2_tc_24">No. of Lauryl Tryptose Broth tube with (+) rxn after 24h</label>
-                <input id="micr2_tc_24" type="number" name="micr2_tc_24" value="{{ old('micr2_tc_24') }}" required
+                <input id="micr2_tc_24" type="number" name="micr2_tc_24" value="{{ $rawDataFileValue->micr2_tc_24 }}" required
                     autofocus autocomplete="micr2_tc_24" onchange="micro2()">
                 @error('micr2_tc_24')
                     <span class="invalid" role="alert">
@@ -17,7 +17,7 @@
 
             <div class="mb-3">
                 <label for="micr2_tc_48">No. of Lauryl Tryptose Broth tube with (+) rxn after 48h</label>
-                <input id="micr2_tc_48" type="number" name="micr2_tc_48" value="{{ old('micr2_tc_48') }}" required
+                <input id="micr2_tc_48" type="number" name="micr2_tc_48" value="{{ $rawDataFileValue->micr2_tc_48 }}" required
                     autofocus autocomplete="micr2_tc_48" onchange="micro2()">
                 @error('micr2_tc_48')
                     <span class="invalid" role="alert">
@@ -29,7 +29,7 @@
             <div class="mb-3">
                 <label for="micr2_tc_final_result">Final Result</label>
                 <input id="micr2_tc_final_result" type="text" name="micr2_tc_final_result"
-                    value="{{ old('micr2_tc_final_result') }}" required autofocus autocomplete="micr2_tc_final_result"
+                    value="{{ $rawDataFileValue->micr2_tc_final_result }}" required autofocus autocomplete="micr2_tc_final_result"
                     readonly>
                 @error('micr2_tc_final_result')
                     <span class="invalid" role="alert">
@@ -41,7 +41,7 @@
             <div class="mb-3">
                 <label for="micr2_tc_remarks">Remarks</label>
                 <input id="micr2_tc_remarks" type="text" name="micr2_tc_remarks"
-                    value="{{ old('micr2_tc_remarks') }}" required autofocus autocomplete="micr2_tc_remarks" readonly>
+                    value="{{ $rawDataFileValue->micr2_tc_remarks }}" required autofocus autocomplete="micr2_tc_remarks" readonly>
                 @error('micr2_tc_remarks')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
