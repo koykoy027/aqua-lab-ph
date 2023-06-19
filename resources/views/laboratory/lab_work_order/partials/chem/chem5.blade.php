749ccd0ae45917e723745e65e4d1d90ca9c5e17f
koykoy027 <villanuevajoshua27@gmail.com>
2023-06-19 14:45:56 +0800

    chem5 fix

--- resources/views/laboratory/lab_work_order/partials/chem/chem5.blade.php
+++ resources/views/laboratory/lab_work_order/partials/chem/chem5.blade.php
@@ -6,8 +6,7 @@
         <div class="sm:gid-cols-1 grid gap-4 lg:grid-cols-2">
             <div class="mb-3">
                 <label for="chem5_instrument_reading_1">Instrument Reading 1</label>
-                <input id="chem5_instrument_reading_1" type="number" name="chem5_instrument_reading_1" value="{{ old('chem5_instrument_reading_1') }}" required autofocus
-                    autocomplete="chem5_instrument_reading_1">
+                <input id="chem5_instrument_reading_1" type="number" name="chem5_instrument_reading_1" value="{{ $rawDataFileValue->chem5_instrument_reading_1 }}" required autofocus autocomplete="chem5_instrument_reading_1">
                 @error('chem5_instrument_reading_1')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -17,8 +16,7 @@
 
             <div class="mb-3">
                 <label for="chem5_instrument_reading_2">Instrument Reading 2</label>
-                <input id="chem5_instrument_reading_2" type="number" name="chem5_instrument_reading_2" value="{{ old('chem5_instrument_reading_2') }}" required
-                    autofocus autocomplete="chem5_instrument_reading_2">
+                <input id="chem5_instrument_reading_2" type="number" name="chem5_instrument_reading_2" value="{{ $rawDataFileValue->chem5_instrument_reading_2 }}" required autofocus autocomplete="chem5_instrument_reading_2">
                 @error('chem5_instrument_reading_2')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -28,8 +26,7 @@
 
             <div class="mb-3">
                 <label for="chem5_instrument_reading_3">Instrument Reading 3</label>
-                <input id="chem5_instrument_reading_3" type="number" name="chem5_instrument_reading_3" value="{{ old('chem5_instrument_reading_3') }}" required
-                    autofocus autocomplete="chem5_instrument_reading_3">
+                <input id="chem5_instrument_reading_3" type="number" name="chem5_instrument_reading_3" value="{{ $rawDataFileValue->chem5_instrument_reading_3 }}" required autofocus autocomplete="chem5_instrument_reading_3">
                 @error('chem5_instrument_reading_3')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -39,8 +36,7 @@
 
             <div class="mb-3">
                 <label for="chem5_average_instrument_reading">Average Instrument Reading</label>
-                <input id="chem5_average_instrument_reading" type="text" name="chem5_average_instrument_reading" value="{{ old('chem5_average_instrument_reading') }}" required
-                    autofocus autocomplete="chem5_average_instrument_reading" readonly>
+                <input id="chem5_average_instrument_reading" type="text" name="chem5_average_instrument_reading" value="{{ old('chem5_average_instrument_reading') }}" required autofocus autocomplete="chem5_average_instrument_reading" readonly>
                 @error('chem5_average_instrument_reading')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -50,8 +46,7 @@
 
             <div class="mb-3">
                 <label for="chem5_final_result">Final Result</label>
-                <input id="chem5_final_result" type="number" name="chem5_final_result" value="{{ old('chem5_final_result') }}" required
-                    autofocus autocomplete="chem5_final_result">
+                <input id="chem5_final_result" type="number" name="chem5_final_result" value="{{ old('chem5_final_result') }}" required autofocus autocomplete="chem5_final_result">
                 @error('chem5_final_result')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
@@ -61,8 +56,7 @@
 
             <div class="mb-3">
                 <label for="chem5_final_result_remarks">Final Result Remarks</label>
-                <input id="chem5_final_result_remarks" type="text" name="chem5_final_result_remarks"
-                    value="{{ old('chem5_final_result_remarks') }}" required autofocus autocomplete="chem5_final_result_remarks" readonly>
+                <input id="chem5_final_result_remarks" type="text" name="chem5_final_result_remarks" value="{{ old('chem5_final_result_remarks') }}" required autofocus autocomplete="chem5_final_result_remarks" readonly>
                 @error('chem5_final_result_remarks')
                     <span class="invalid" role="alert">
                         <strong>{{ $message }}</strong>
