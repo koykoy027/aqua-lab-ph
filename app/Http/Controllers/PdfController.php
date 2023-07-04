f864e4942950be52382f624373e0e6981cf7da4b
koykoy027 <villanuevajoshua27@gmail.com>
2023-07-04 20:38:07 +0800

    rawr

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\AnalysisRequest;
-
+use App\Models\Client;
+use App\Models\LabAcceptance;
 use App\Models\RawData;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -16,11 +17,13 @@ class PdfController extends Controller
 
         $micro_details = RawData::where('analysis_id',$analysis_id)->get();
 
+        $clients = Client::find($analysis_id);
         $details = AnalysisRequest::find($analysis_id);
+        $labAcceptance = LabAcceptance::find($analysis_id);
 
         $collection_details = AnalysisRequest::where('analysis_id',$analysis_id)->get();
 
-        $pdf = Pdf::loadView('record_and_report.analysis_request.pdf', ['details' =>  $details, 'micro_details' =>  $micro_details, 'collection_details' => $collection_details])->setPaper([0, 0, 612, 1008], 'portrait');
+        $pdf = Pdf::loadView('record_and_report.analysis_request.pdf', ['details' =>  $details, 'micro_details' =>  $micro_details, 'collection_details' => $collection_details, 'clients' => $clients, 'labAcceptance' => $labAcceptance])->setPaper([0, 0, 612, 1008], 'portrait');
         return $pdf->stream('analysis-request.pdf');
 
     }
--- resources/views/record_and_report/analysis_request/pdf.blade.php
+++ resources/views/record_and_report/analysis_request/pdf.blade.php
@@ -16,11 +16,11 @@
         .title {
             text-transform: uppercase;
             font-size: 16px;
-            font-weight: 400; 
+            font-weight: 400;
             text-align: center;
             margin-top: 80px;
         }
-    
+
         .table-container {
             margin-left: auto;
             margin-right: auto;
@@ -98,7 +98,7 @@
             bottom: 100;
             /* width: 100%; */
         }
-        
+
 
     </style>
 </head>
@@ -119,29 +119,29 @@
                         <p>Account ID: {{ $details->account_number }} </p>
                         <p>Sample ID: {{ $details->analysis_id }}</p>
                     </td>
-                </tr> 
+                </tr>
             </thead>
         </table>
 
         <table style="width: 100%; margin-top: 30px">
             <thead>
                 <tr class="top-information">
-                    <td>Requested By:</td> 
+                    <td>Requested By:</td>
                 </tr>
 
                 <tr class="top-information">
                     <td>Main Source: <span class="main-source">{{ $details->source_of_water_sample }}</span></td>
-                    <td>Sampling point:</td>
+                    <td>Sampling point: <span class="main-source">{{ $details->collection_point }}</td>
                 </tr>
 
                 <tr class="top-information">
                     <td>Water Purpose (Use): <span class="top-information-content">{{ $details->water_purpose }}</span></td>
-                    <td>Type of Water:</td>
+                    <td>Type of Water: <span class="top-information-content">{{ $details->water_purpose }}</span></td>
                 </tr>
 
                 <tr class="top-information">
-                    <td>Date/Time Collected: <span class="top-information-content">{{ $details->date_collected." ".$details->time_collected }}</span></td>
-                    <td>Date/Time Received:</td>
+                    <td>Date/Time Collected: <span class="top-information-content">{{ $labAcceptance->date_evaluated }} {{ $labAcceptance->date_evaluated }}</span></td>
+                    <td>Date/Time Received: <span class="top-information-content">-</span></td>
                 </tr>
 
                 <tr class="top-information">
@@ -152,7 +152,7 @@
             </thead>
         </table>
     </div>
-    
+
     <div class="title">certificate of microbiological analysis</div>
 
     <table class="table-container">
@@ -314,7 +314,7 @@
                             <td class="details"></td>
                             <td class="details">{{ $micro_detail->chem10_final_result_remarks }}</td>
 
-                        {{-- phys 1 --}}  
+                        {{-- phys 1 --}}
                         @elseif ( $collection_detail->test_parameters == "PHYS1 - Appearance" )
 
                             <td class="details">{{ $collection_detail->test_parameters }}</td>
@@ -355,13 +355,13 @@
                 </tr>
             @endforeach
         </tbody>
-    </table> 
+    </table>
 
     <table tyle="width: 100%">
         <thead>
             <tr>
                 <td style="font-size:  13px">Remarks:</td>
-                <td style="padding-left: 200px; padding-right:50px; font-size:  13px"> 
+                <td style="padding-left: 200px; padding-right:50px; font-size:  13px">
                     <p>
                         Results of examination are specifically related to samples as received
                     </p>
@@ -371,7 +371,7 @@
             <tr>
                 <td></td>
                 <td style="padding-left: 200px; padding-right:50px; font-size:  13px">
-                    <p style="margin-top: -10px; font-weight: 500; text-align: justify;">Pursuant to PNSDW 2017, sample was collected according to prescribed aseptic technique 
+                    <p style="margin-top: -10px; font-weight: 500; text-align: justify;">Pursuant to PNSDW 2017, sample was collected according to prescribed aseptic technique
                         and was contained and transported in a sterilized container at controlled temperature
                         by Aqualab PH trained personnel.
                     </p>
@@ -400,9 +400,9 @@
                 <td></td>
                 <td style="padding-left: 200px; font-size:  13px; padding-right:50px;">
                     <p style="margin-top: -15px; text-align: justify;">
-                        Water and Wastewater (SMEWW) American Water Works Association, 22nd Edition (2012); Parameters and 
+                        Water and Wastewater (SMEWW) American Water Works Association, 22nd Edition (2012); Parameters and
                         Limits are based on Philippine National Standards for Drinking Water (2017) Thermotolerant Coliform - also
-                        Fecal Coliform; MPN/100ml - Most Probable Number per 100 ml of sample; cfu - Colony Forming Unit per 1mL of sample 
+                        Fecal Coliform; MPN/100ml - Most Probable Number per 100 ml of sample; cfu - Colony Forming Unit per 1mL of sample
                     </p>
                 </td>
             </tr>
@@ -429,7 +429,7 @@
                         <p style="margin-top: -3%; margin-left: 4%;"> PAM Reg. No. 105-00250 RM </p>
                         <p style="margin-top: -3%; margin-left: 1px;"> DOH-RL Cert No. WMLA-18-0698 </p>
                     </td>
-    
+
                     <td style="font-size:  13px; padding-left: 200px; margin-top; -200px;">
                         <p style="font-weight: 500;"> PAULO ANTONIO E. CLEMENTE, MD, DPSP </p>
                         <p style="margin-right: 10%; text-align: center;"> Head of Laboratory </p>
