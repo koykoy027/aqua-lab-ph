921eb1918f0c4282b071cf1a3196fa89ada4db90
Jhunriz <lalata.jhunriz.bscs2019@gmail.com>
2023-07-06 21:42:16 +0800

    chem1-done

--- public/js/app.js
+++ public/js/app.js
@@ -1,297 +1,318 @@
 // can be seen in service.client.create
 const marketSegment = (val) => {
-    var input = document.getElementById('market_segment_others');
+    var input = document.getElementById("market_segment_others");
 
     if (val === "Others") {
         // actve readonly
-        input.removeAttribute('readonly');
-    }
-    else {
+        input.removeAttribute("readonly");
+    } else {
         // disable readonly
-        input.setAttribute('readonly', 'readonly');
-        input.value = '';
+        input.setAttribute("readonly", "readonly");
+        input.value = "";
     }
-}
+};
 
 // can be seen in service.add-analysis-request-form
 const collectionPoint = (val) => {
-    var input = document.getElementById('collection_point_others');
+    var input = document.getElementById("collection_point_others");
     if (val === "Others") {
         // actve readonly
 
-        input.removeAttribute('readonly');
-    }
-    else {
+        input.removeAttribute("readonly");
+    } else {
         // disable readonly
-        input.setAttribute('readonly', 'readonly');
-        input.value = '';
+        input.setAttribute("readonly", "readonly");
+        input.value = "";
     }
-}
+};
 
 // can be seen in service.add-analysis-request-form
 const samplingLocationAddress = (val) => {
-    var input = document.getElementById('sampling_location_address_others');
+    var input = document.getElementById("sampling_location_address_others");
     if (val === "Others") {
         // actve readonly
-        input.removeAttribute('readonly');
-    }
-    else {
+        input.removeAttribute("readonly");
+    } else {
         // disable readonly
-        input.setAttribute('readonly', 'readonly');
-        input.value = '';
+        input.setAttribute("readonly", "readonly");
+        input.value = "";
     }
-}
+};
 
 // can be seen in service.add-analysis-request-form
 const sourceOfWaterSample = (val) => {
-    var input = document.getElementById('source_of_water_sample_others');
+    var input = document.getElementById("source_of_water_sample_others");
     if (val === "Others") {
         // actve readonly
-        input.removeAttribute('readonly');
-    }
-    else {
+        input.removeAttribute("readonly");
+    } else {
         // disable readonly
-        input.setAttribute('readonly', 'readonly');
-        input.value = '';
+        input.setAttribute("readonly", "readonly");
+        input.value = "";
     }
-}
+};
 
 // can be seen in service.add-analysis-request-form
 const waterPurpose = (val) => {
-    var input = document.getElementById('water_purpose_others');
+    var input = document.getElementById("water_purpose_others");
     if (val === "Others") {
         // actve readonly
-        input.removeAttribute('readonly');
-    }
-    else {
+        input.removeAttribute("readonly");
+    } else {
         // disable readonly
-        input.setAttribute('readonly', 'readonly');
-        input.value = '';
+        input.setAttribute("readonly", "readonly");
+        input.value = "";
     }
-}
+};
 
 // can be seen in laboratory.lab-acceptance-form
 const remarksRejected = (radio) => {
-    var input = document.getElementById('if_remarks_are_rejected');
+    var input = document.getElementById("if_remarks_are_rejected");
 
-    if (radio.checked && radio.value === 'Rejected') {
-        input.removeAttribute('readonly');
+    if (radio.checked && radio.value === "Rejected") {
+        input.removeAttribute("readonly");
     } else {
-        input.setAttribute('readonly', 'readonly');
-        input.value = '';
+        input.setAttribute("readonly", "readonly");
+        input.value = "";
     }
-}
+};
 
 // can be seen in laboratory.lab_work_order-partials
 const micro1 = () => {
-    var micr1_hpc_plate_a = document.querySelector('[name="micr1_hpc_plate_a"]').value;
-    var micr1_hpc_plate_b = document.querySelector('[name="micr1_hpc_plate_b"]').value;
-    var micr1_hpc_average = document.querySelector('[name="micr1_hpc_average"]');
-    var micr1_hpc_difference = document.querySelector('[name="micr1_hpc_difference"]');
-    var micr1_hpc_remarks = document.querySelector('[name="micr1_hpc_remarks"]');
-    var micr1_hpc_final_result = document.querySelector('[name="micr1_hpc_final_result"]');
+    var micr1_hpc_plate_a = document.querySelector(
+        '[name="micr1_hpc_plate_a"]'
+    ).value;
+    var micr1_hpc_plate_b = document.querySelector(
+        '[name="micr1_hpc_plate_b"]'
+    ).value;
+    var micr1_hpc_average = document.querySelector(
+        '[name="micr1_hpc_average"]'
+    );
+    var micr1_hpc_difference = document.querySelector(
+        '[name="micr1_hpc_difference"]'
+    );
+    var micr1_hpc_remarks = document.querySelector(
+        '[name="micr1_hpc_remarks"]'
+    );
+    var micr1_hpc_final_result = document.querySelector(
+        '[name="micr1_hpc_final_result"]'
+    );
 
     if (micr1_hpc_plate_a.length > 0 && micr1_hpc_plate_b.length > 0) {
         // COMPUTE Average
-        micr1_hpc_average.value = Math.ceil((parseFloat(micr1_hpc_plate_a) + parseFloat(micr1_hpc_plate_b)) / 2);
+        micr1_hpc_average.value = Math.ceil(
+            (parseFloat(micr1_hpc_plate_a) + parseFloat(micr1_hpc_plate_b)) / 2
+        );
 
         // COMPUTE Difference
-        micr1_hpc_difference.value = Math.ceil(parseFloat(((parseFloat(micr1_hpc_plate_a) - parseFloat(micr1_hpc_plate_b)) /
-            ((parseFloat(micr1_hpc_plate_a) + parseFloat(micr1_hpc_plate_b)) / 2) * 100).toFixed(2)));
+        micr1_hpc_difference.value = Math.ceil(
+            parseFloat(
+                (
+                    ((parseFloat(micr1_hpc_plate_a) -
+                        parseFloat(micr1_hpc_plate_b)) /
+                        ((parseFloat(micr1_hpc_plate_a) +
+                            parseFloat(micr1_hpc_plate_b)) /
+                            2)) *
+                    100
+                ).toFixed(2)
+            )
+        );
 
         if (micr1_hpc_average.value >= 499) {
-            micr1_hpc_remarks.value = 'FAIL';
-            micr1_hpc_final_result.value = '≥ 500 est';
+            micr1_hpc_remarks.value = "FAIL";
+            micr1_hpc_final_result.value = "≥ 500 est";
         } else {
-            micr1_hpc_remarks.value = 'PASSED';
-            micr1_hpc_final_result.value = '≤ 500 est';
+            micr1_hpc_remarks.value = "PASSED";
+            micr1_hpc_final_result.value = "≤ 500 est";
         }
     } else {
-        micr1_hpc_average.value = '';
-        micr1_hpc_difference.value = '';
-        micr1_hpc_remarks.value = '';
-        micr1_hpc_final_result.value = '';
+        micr1_hpc_average.value = "";
+        micr1_hpc_difference.value = "";
+        micr1_hpc_remarks.value = "";
+        micr1_hpc_final_result.value = "";
     }
-}
+};
 
 // can be seen in laboratory.lab_work_order-partials
 const micro2 = () => {
     var micr2_tc_24 = document.querySelector('[name="micr2_tc_24"]').value;
     var micr2_tc_48 = document.querySelector('[name="micr2_tc_48"]').value;
-    var micr2_tc_final_result = document.querySelector('[name="micr2_tc_final_result"]');
+    var micr2_tc_final_result = document.querySelector(
+        '[name="micr2_tc_final_result"]'
+    );
     var micr2_tc_remarks = document.querySelector('[name="micr2_tc_remarks"]');
 
     if (micr2_tc_24.length > 0 && micr2_tc_48.length > 0) {
         if (micr2_tc_24 == 0) {
-            micr2_tc_final_result.value = '< 1.1';
+            micr2_tc_final_result.value = "< 1.1";
         } else if (micr2_tc_24 == 1) {
-            micr2_tc_final_result.value = '1.1';
+            micr2_tc_final_result.value = "1.1";
         } else if (micr2_tc_24 == 2) {
-            micr2_tc_final_result.value = '2.6';
+            micr2_tc_final_result.value = "2.6";
         } else if (micr2_tc_24 == 3) {
-            micr2_tc_final_result.value = '4.6';
+            micr2_tc_final_result.value = "4.6";
         } else if (micr2_tc_24 == 4) {
-            micr2_tc_final_result.value = '8.0';
+            micr2_tc_final_result.value = "8.0";
         } else if (micr2_tc_24 >= 5) {
-            micr2_tc_final_result.value = '> 8.0';
+            micr2_tc_final_result.value = "> 8.0";
         }
 
-        if (micr2_tc_final_result.value == '< 1.1') {
-            micr2_tc_remarks.value = 'PASSED';
+        if (micr2_tc_final_result.value == "< 1.1") {
+            micr2_tc_remarks.value = "PASSED";
         } else {
-            micr2_tc_remarks.value = 'FAILED';
+            micr2_tc_remarks.value = "FAILED";
         }
     } else {
-        micr2_tc_final_result.value = '';
-        micr2_tc_remarks.value = '';
+        micr2_tc_final_result.value = "";
+        micr2_tc_remarks.value = "";
     }
-}
+};
 
 // can be seen in laboratory.lab_work_order-partials
 const micro3 = () => {
-
-    var micr3_lauryl_24 = document.querySelector('[name="micr3_lauryl_24"]').value;
-    var micr3_lauryl_48 = document.querySelector('[name="micr3_lauryl_48"]').value;
-    var micr3_brillant_24 = document.querySelector('[name="micr3_brillant_24"]').value;
-    var micr3_brillant_48 = document.querySelector('[name="micr3_brillant_48"]').value;
-    var micr3_final_result = document.querySelector('[name="micr3_final_result"]');
+    var micr3_lauryl_24 = document.querySelector(
+        '[name="micr3_lauryl_24"]'
+    ).value;
+    var micr3_lauryl_48 = document.querySelector(
+        '[name="micr3_lauryl_48"]'
+    ).value;
+    var micr3_brillant_24 = document.querySelector(
+        '[name="micr3_brillant_24"]'
+    ).value;
+    var micr3_brillant_48 = document.querySelector(
+        '[name="micr3_brillant_48"]'
+    ).value;
+    var micr3_final_result = document.querySelector(
+        '[name="micr3_final_result"]'
+    );
     var micr3_remarks = document.querySelector('[name="micr3_remarks"]');
 
-
     if (micr3_lauryl_48.length > 0) {
-
         if (micr3_lauryl_48 == 0) {
-            micr3_final_result.value = '< 1.1';
-        }
-        else if (micr3_lauryl_48 == 1) {
-            micr3_final_result.value = '1.1';
-
-        }
-        else if (micr3_lauryl_48 == 2) {
-            micr3_final_result.value = '2.6';
-
-        }
-        else if (micr3_lauryl_48 == 3) {
-            micr3_final_result.value = '4.6';
-
+            micr3_final_result.value = "< 1.1";
+        } else if (micr3_lauryl_48 == 1) {
+            micr3_final_result.value = "1.1";
+        } else if (micr3_lauryl_48 == 2) {
+            micr3_final_result.value = "2.6";
+        } else if (micr3_lauryl_48 == 3) {
+            micr3_final_result.value = "4.6";
         } else if (micr3_lauryl_48 == 4) {
-            micr3_final_result.value = '8.0';
-
+            micr3_final_result.value = "8.0";
         } else if (micr3_lauryl_48 >= 5) {
-            micr3_final_result.value = '> 8.0';
+            micr3_final_result.value = "> 8.0";
         }
 
-        if (micr3_final_result == '< 1.1') {
-            micr3_remarks.value = 'PASSED';
+        if (micr3_final_result == "< 1.1") {
+            micr3_remarks.value = "PASSED";
         } else {
-            micr3_remarks.value = 'FAILED';
+            micr3_remarks.value = "FAILED";
         }
-
     } else {
-        micr3_final_result.value = '';
-        micr3_remarks.value = '';
+        micr3_final_result.value = "";
+        micr3_remarks.value = "";
     }
-
-}
+};
 
 // can be seen in laboratory.lab_work_order-partials
 
 const chem1 = () => {
+    var chem1_instrument_reading_1 = document.querySelector(
+        '[name="chem1_instrument_reading_1"]'
+    ).value;
+    var chem1_instrument_reading_2 = document.querySelector(
+        '[name="chem1_instrument_reading_2"]'
+    ).value;
+    var chem1_instrument_reading_3 = document.querySelector(
+        '[name="chem1_instrument_reading_3"]'
+    ).value;
+    var chem1_average_instrument_reading = document.querySelector(
+        '[name="chem1_average_instrument_reading"]'
+    );
+    var chem1_final_result = document.querySelector(
+        '[name="chem1_final_result"]'
+    );
+    var chem1_final_result_remarks = document.querySelector(
+        '[name="chem1_final_result_remarks"]'
+    );
 
-
-    var chem1_instrument_reading_1 = document.querySelector('[name="chem1_instrument_reading_1"]').value
-    var chem1_instrument_reading_2 = document.querySelector('[name="chem1_instrument_reading_2"]').value
-    var chem1_instrument_reading_3 = document.querySelector('[name="chem1_instrument_reading_3"]').value
-    var chem1_average_instrument_reading = document.querySelector('[name="chem1_average_instrument_reading"]')
-    var chem1_final_result = document.querySelector('[name="chem1_final_result"]')
-    var chem1_final_result_remarks = document.querySelector('[name="chem1_final_result_remarks"]')
-
-    var reading1 = parseFloat(chem1_instrument_reading_1)
-    var reading2 = parseFloat(chem1_instrument_reading_2)
-    var reading3 = parseFloat(chem1_instrument_reading_3)
+    var reading1 = parseFloat(chem1_instrument_reading_1);
+    var reading2 = parseFloat(chem1_instrument_reading_2);
+    var reading3 = parseFloat(chem1_instrument_reading_3);
 
     var averageReading = (reading1 + reading2 + reading3) / 3;
 
-
-
-    if (chem1_average_instrument_reading.length > 0) {
-
-        if (chem1_final_result == 0) {
-            chem1_final_result.value = '< 1.1';
-        }
-        else if (chem1_final_result == 1) {
-            chem1_final_result.value = '1.1';
-
-        }
-        else if (chem1_final_result == 2) {
-            chem1_final_result.value = '2.6';
-
-        }
-        else if (chem1_final_result == 3) {
-            chem1_final_result.value = '4.6';
-
-        } else if (chem1_final_result == 4) {
-            chem1_final_result.value = '8.0';
-
-        } else if (chem1_final_result >= 5) {
-            chem1_final_result.value = '> 8.0';
-        }
-
-        if (chem1_final_result == '< 1.1') {
-            chem1_final_result_remarks.value = 'PASSED';
-        } else {
-            chem1_final_result_remarks.value = 'FAILED';
-        }
-
+    if (averageReading < 6.5 || averageReading > 8.5) {
+        chem1_final_result_remarks.value = "FAIL";
     } else {
-        chem1_average_instrument_reading.value = averageReading;
+        chem1_final_result_remarks.value = "PASS";
     }
 
-
-
-
-
-
-
-
-
-
-
-
-}
-
+    if (averageReading < 1.1) {
+        chem1_final_result.value = averageReading.toFixed(1);
+        chem1_average_instrument_reading.value = averageReading.toFixed(1);
+    } else if (averageReading < 2.6) {
+        chem1_final_result.value = averageReading.toFixed(1);
+        chem1_average_instrument_reading.value = averageReading.toFixed(1);
+    } else if (averageReading < 4.6) {
+        chem1_final_result.value = averageReading.toFixed(1);
+        chem1_average_instrument_reading.value = averageReading.toFixed(1);
+    } else if (averageReading < 8.0) {
+        chem1_final_result.value = averageReading.toFixed(1);
+        chem1_average_instrument_reading.value = averageReading.toFixed(1);
+    } else {
+        chem1_final_result.value = averageReading.toFixed(1);
+        chem1_average_instrument_reading.value = averageReading.toFixed(1);
+    }
+};
 
 const handleCategoryClick = (category, selector) => {
-    const buttons = document.querySelectorAll('.category-button');
+    const buttons = document.querySelectorAll(".category-button");
     buttons.forEach((button) => {
-        button.classList.remove('text-blue-500', 'border-b-2', 'border-blue-500');
+        button.classList.remove(
+            "text-blue-500",
+            "border-b-2",
+            "border-blue-500"
+        );
     });
 
     const clickedButton = document.querySelector(selector);
-    clickedButton.classList.add('text-blue-500', 'border-b-2', 'border-blue-500');
+    clickedButton.classList.add(
+        "text-blue-500",
+        "border-b-2",
+        "border-blue-500"
+    );
 
-    const categories = document.querySelectorAll('.category');
+    const categories = document.querySelectorAll(".category");
     categories.forEach((categoryElement) => {
         if (categoryElement.classList.contains(category)) {
-            categoryElement.style.display = 'block';
+            categoryElement.style.display = "block";
         } else {
-            categoryElement.style.display = 'none';
+            categoryElement.style.display = "none";
         }
     });
 };
 
 const categoryClientInformation = () => {
-    handleCategoryClick('categoryClientInformation', '[onclick="categoryClientInformation()"]');
+    handleCategoryClick(
+        "categoryClientInformation",
+        '[onclick="categoryClientInformation()"]'
+    );
 };
 
 const categoryAnalysisRequest = () => {
-    handleCategoryClick('categoryAnalysisRequest', '[onclick="categoryAnalysisRequest()"]');
+    handleCategoryClick(
+        "categoryAnalysisRequest",
+        '[onclick="categoryAnalysisRequest()"]'
+    );
 };
 
 const categoryLabAcceptance = () => {
-    handleCategoryClick('categoryLabAcceptance', '[onclick="categoryLabAcceptance()"]');
+    handleCategoryClick(
+        "categoryLabAcceptance",
+        '[onclick="categoryLabAcceptance()"]'
+    );
 };
 
 const categoryRawData = () => {
-    handleCategoryClick('categoryRawData', '[onclick="categoryRawData()"]');
+    handleCategoryClick("categoryRawData", '[onclick="categoryRawData()"]');
 };
