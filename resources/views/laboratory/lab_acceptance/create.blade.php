9fad34387cb4f365573c1fe3924263f8c93c71b9
koykoy027 <villanuevajoshua27@gmail.com>
2023-08-04 16:50:19 +0800

    change Approve into Submit

--- resources/views/laboratory/lab_acceptance/create.blade.php
+++ resources/views/laboratory/lab_acceptance/create.blade.php
@@ -209,7 +209,7 @@
 
 
         <div class="flex justify-end">
-            <button class="btn btn-primary" type="submit">Approve</button>
+            <button class="btn btn-primary" type="submit">Submit</button>
         </div>
     </form>
 
