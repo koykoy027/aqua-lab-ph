c2a588763f1b78dd4190e5909eb2854e14fd349b
KevinCasoco <andreikevincasoco@gmail.com>
2023-09-11 13:23:21 +0800

    micro_completed

--- app/Http/Controllers/LabApprovalController.php
+++ app/Http/Controllers/LabApprovalController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\AnalysisRequest;
 use App\Models\LabAcceptance;
+use App\Models\LibraryTestParameter;
+use App\Models\Micro1;
+use App\Models\Micro2;
+use App\Models\Micro3;
+use App\Models\Micro4;
 use App\Models\RawData;
+use App\Models\TestParameter;
 use Illuminate\Http\Request;
 
 class LabApprovalController extends Controller
@@ -41,15 +47,34 @@ class LabApprovalController extends Controller
         return view('laboratory.lab_approval.index', compact('requests', 'query'));
     }
 
-
-
     public function details($analysis_id)
     {
-        $details = AnalysisRequest::find($analysis_id);
 
-        $lab_approval = RawData::find($details->analysis_id);
+        $requests = LabAcceptance::find($analysis_id);
+        $lab_approval = AnalysisRequest::find($analysis_id);
+        $rawDataFileValue = RawData::find($analysis_id);
 
-        return view('laboratory.lab_approval.details', compact('details', 'lab_approval'));
+        $micro1 = Micro1::where('analysis_id',$analysis_id)->get();
+        $micro2 = Micro2::where('analysis_id',$analysis_id)->get();
+        $micro3 = Micro3::where('analysis_id',$analysis_id)->get();
+        $micro4 = Micro4::where('analysis_id',$analysis_id)->get();
+
+        $test_parameters = TestParameter::query()
+        ->where('analysis_id', $analysis_id)
+        ->get();
+
+        $library_test_parameters = LibraryTestParameter::all();
+
+        return view('laboratory.lab_approval.details', compact
+        ('requests',
+        'lab_approval',
+        'test_parameters',
+        'library_test_parameters',
+        'micro1',
+        'micro2',
+        'micro3',
+        'micro4'));
+
     }
 
     public function approval($analysis_id)
--- resources/views/laboratory/lab_approval/partials/micro/micr4.blade.php
+++ resources/views/laboratory/lab_approval/partials/micro/micr4.blade.php
@@ -1,20 +1,22 @@
 <div class="card mb-3 bg-white">
     <h1 class="mb-3">MICR4 - E. coli Test</h1>
     <div class="sm:gid-cols-1 grid gap-4 lg:grid-cols-2">
+        @foreach ($micro4 as $micro4s)
         <div class="mb-3">
             <label for="micr4_color_of_the_sample">Color of the sample</label>
-            <input id="micr4_color_of_the_sample" type="number" name="micr4_color_of_the_sample" value="{{ $lab_approval->micr4_color_of_the_sample }}" readonly autofocus autocomplete="micr4_color_of_the_sample" />
+            <input id="micr4_color_of_the_sample" type="text" name="micr4_color_of_the_sample" value="{{ $micro4s->micr4_color_of_the_sample }}" readonly autofocus autocomplete="micr4_color_of_the_sample" />
 
         </div>
 
         <div class="mb-3">
             <label for="micr4_fluorescence">Fluorescence</label>
-            <input id="micr4_fluorescence" type="number" name="micr4_fluorescence" value="{{ $lab_approval->micr4_fluorescence }}" readonly autofocus autocomplete="micr4_fluorescence" />
+            <input id="micr4_fluorescence" type="text" name="micr4_fluorescence" value="{{ $micro4s->micr4_fluorescence }}" readonly autofocus autocomplete="micr4_fluorescence" />
         </div>
 
         <div class="mb-3">
             <label for="micr4_final_result">Final Result</label>
-            <input id="micr4_final_result" type="text" name="micr4_final_result" value="{{ $lab_approval->micr4_final_result }}" readonly autofocus autocomplete="micr4_final_result" />
+            <input id="micr4_final_result" type="text" name="micr4_final_result" value="{{ $micro4s->micr4_final_result }}" readonly autofocus autocomplete="micr4_final_result" />
         </div>
     </div>
+    @endforeach
 </div>
