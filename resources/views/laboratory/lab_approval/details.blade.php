9dd93109954c765de31af688b4ae2cfca558ccbd
koykoy027 <villanuevajoshua27@gmail.com>
2023-09-12 11:55:45 +0800

    remove comments

--- resources/views/laboratory/lab_approval/details.blade.php
+++ resources/views/laboratory/lab_approval/details.blade.php
@@ -2,84 +2,6 @@
 @section('title', 'Lab Approval Details')
 @section('content')
 
-{{-- @if ($details->test_parameters == 'MICR1 - Heterotrophic Plate Count (HPC)')
-@include('laboratory.lab_approval.partials.micro.micr1')
-@endif
-
-@if ($details->test_parameters == 'MICR2 - Thermotolerant Colifom Test')
-@include('laboratory.lab_approval.partials.micro.micr2')
-@endif
-
-@if ($details->test_parameters == 'MICR3 - Total Coliform')
-@include('laboratory.lab_approval.partials.micro.micr3')
-@endif
-
-@if ($details->test_parameters == 'MICR4 - E. coli Test')
-@include('laboratory.lab_approval.partials.micro.micr4')
-@endif
-
-@if ($details->test_parameters == 'MICR5 - All three (3) Mandatory Microbiological Parameters (PNSDW 2017/DOH AO 2013-003)')
-    @include('laboratory.lab_approval.partials.micro.micr1')
-    @include('laboratory.lab_approval.partials.micro.micr2')
-    @include('laboratory.lab_approval.partials.micro.micr3')
-@endif
-
-@if ($details->test_parameters == 'CHEM1 - pH')
-    @include('laboratory.lab_approval.partials.chem.chem1')
-@endif
-
-@if ($details->test_parameters == 'CHEM2 - Nitrate')
-    @include('laboratory.lab_approval.partials.chem.chem2')
-@endif
-
-@if ($details->test_parameters == 'CHEM3 - Total Dissolved Solids')
-    @include('laboratory.lab_approval.partials.chem.chem3')
-@endif
-
-@if ($details->test_parameters == 'CHEM4 - Chlorine (Residual), as free')
-    @include('laboratory.lab_approval.partials.chem.chem4')
-@endif
-
-@if ($details->test_parameters == 'CHEM5 - Arsenic')
-    @include('laboratory.lab_approval.partials.chem.chem5')
-@endif
-
-@if ($details->test_parameters == 'CHEM6 - Cadmium')
-    @include('laboratory.lab_approval.partials.chem.chem6')
-@endif
-
-@if ($details->test_parameters == 'CHEM7 - Lead')
-    @include('laboratory.lab_approval.partials.chem.chem7')
-@endif
-
-@if ($details->test_parameters == 'CHEM8 - All Twenty (20) Mandatory Chemical Parameters (DOH AO 2013-003 Dialysis Water)')
-    {{-- @include('laboratory.lab_approval.partials.chem.chem1')
-@endif
-
-@if ($details->test_parameters == 'CHEM9 - Iron')
-    @include('laboratory.lab_approval.partials.chem.chem9')
-@endif
-
-@if ($details->test_parameters == 'CHEM10 - Manganese')
-    @include('laboratory.lab_approval.partials.chem.chem10')
-@endif
-
-@if ($details->test_parameters == 'PHYS1 - Appearance')
-    @include('laboratory.lab_approval.partials.phys.phys1')
-@endif
-
-@if ($details->test_parameters == 'PHYS2 - Odor')
-    @include('laboratory.lab_approval.partials.phys.phys2')
-@endif
-
-@if ($details->test_parameters == 'PHYS3 - Color, apparent')
-    @include('laboratory.lab_approval.partials.phys.phys3')
-@endif
-
-@if ($details->test_parameters == 'PHYS4 - Turbidity')
-    @include('laboratory.lab_approval.partials.phys.phys4')
-@endif --}}
-
 <div class="grid gap-4 sm:gid-cols-1 lg:grid-cols-2">
     <div class="mb-3 bg-white card">
         <label for="sampleID">Sample ID</label>
@@ -87,19 +9,18 @@
     </div>
 </div>
 
-
 {{-- micro --}}
 @if ($lab_approval->test_parameters === 'micro')
-        <form action="{{ route('laboratory.lab_approval.details', ['analysis_id' => $lab_approval->analysis_id]) }}" method="POST">
-            @csrf
-            @foreach ($test_parameters as $test_parameter)
-                @foreach ($library_test_parameters as $library_test_parameter)
-                    @if ($test_parameter->test_parameters === $library_test_parameter->id)
-                        @include('laboratory.lab_approval.partials.micro.' . $library_test_parameter->filename)
-                    @endif
-                @endforeach
+    <form action="{{ route('laboratory.lab_approval.details', ['analysis_id' => $lab_approval->analysis_id]) }}" method="POST">
+        @csrf
+        @foreach ($test_parameters as $test_parameter)
+            @foreach ($library_test_parameters as $library_test_parameter)
+                @if ($test_parameter->test_parameters === $library_test_parameter->id)
+                    @include('laboratory.lab_approval.partials.micro.' . $library_test_parameter->filename)
+                @endif
             @endforeach
-        </form>
+        @endforeach
+    </form>
 @endif
 
 {{-- pychem --}}
