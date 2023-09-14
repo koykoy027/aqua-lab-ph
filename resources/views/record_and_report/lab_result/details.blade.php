b889f5e694cdba0f2ee63915b0228e884e18373a
KevinCasoco <andreikevincasoco@gmail.com>
2023-09-14 19:20:39 +0800

    tapos_ang_ojt

--- resources/views/record_and_report/lab_result/details.blade.php
+++ resources/views/record_and_report/lab_result/details.blade.php
@@ -37,6 +37,13 @@
                 @include('record_and_report.lab_result.partials.lab_acceptance')
             </div>
 
+            <div class="grid gap-4 sm:gid-cols-1 lg:grid-cols-2">
+                <div class="mb-3 bg-white card">
+                    <label for="sampleID">ANALYSIS REQUEST ID</label>
+                    <input id="sampleID" type="text" value="{{ $analysis->analysis_id_ }}" readonly>
+                </div>
+            </div>
+
             <div class="category categoryRawData hidden">
                 @if ($analysis->test_parameters === 'micro')
                     @foreach ($test_parameters as $test_parameter)
@@ -58,15 +65,15 @@
                     @endforeach
                 @endif
 
-                {{-- @if ($analysis->test_parameters === 'pychem')
+                @if ($analysis->test_parameters === 'pychem')
                 @foreach ($test_parameters as $test_parameter)
                     @foreach ($library_test_parameters as $library_test_parameter)
                         @if ($test_parameter->test_parameters === $library_test_parameter->id)
-                            @include('record_and_report.lab_result.partials.raw_data_partials.phys.' . $library_test_parameter->filename)
+                            @include('record_and_report.lab_result.partials.raw_data_partials.chem.' . $library_test_parameter->filename)
                         @endif
                     @endforeach
                 @endforeach
-                @endif --}}
+                @endif
 
             </div>
         </div>
