4ecf1db116434a10c67287fedb7e9a82cbc7533b
KevinCasoco <andreikevincasoco@gmail.com>
2023-09-14 11:35:19 +0800

    raw_data_file_completed

--- resources/views/record_and_report/lab_result/partials/raw_data_partials/chem/chem9.blade.php
+++ resources/views/record_and_report/lab_result/partials/raw_data_partials/chem/chem9.blade.php
@@ -1,34 +1,36 @@
     <div class="card mb-3 bg-white">
         <h1 class="mb-3">CHEM9 - Iron</h1>
         <div class="sm:gid-cols-1 grid gap-4 lg:grid-cols-2">
-            <div class="mb-3">
-                <label for="chem9_instrument_reading_1">Instrument Reading 1</label>
-                <input id="chem9_instrument_reading_1" type="number" name="chem9_instrument_reading_1" value="{{ $rawData->chem9_instrument_reading_1 }}" required autofocus autocomplete="chem9_instrument_reading_1">
-            </div>
+            @foreach ($chem9 as $chem9s)
+                <div class="mb-3">
+                    <label for="chem9_instrument_reading_1">Instrument Reading 1</label>
+                    <input id="chem9_instrument_reading_1" type="number" name="chem9_instrument_reading_1" value="{{ $chem9s->chem9_instrument_reading_1 }}" required autofocus autocomplete="chem9_instrument_reading_1">
+                </div>
 
-            <div class="mb-3">
-                <label for="chem9_instrument_reading_2">Instrument Reading 2</label>
-                <input id="chem9_instrument_reading_2" type="number" name="chem9_instrument_reading_2" value="{{ $rawData->chem9_instrument_reading_2 }}" required autofocus autocomplete="chem9_instrument_reading_2">
-            </div>
+                <div class="mb-3">
+                    <label for="chem9_instrument_reading_2">Instrument Reading 2</label>
+                    <input id="chem9_instrument_reading_2" type="number" name="chem9_instrument_reading_2" value="{{ $chem9s->chem9_instrument_reading_2 }}" required autofocus autocomplete="chem9_instrument_reading_2">
+                </div>
 
-            <div class="mb-3">
-                <label for="chem9_instrument_reading_3">Instrument Reading 3</label>
-                <input id="chem9_instrument_reading_3" type="number" name="chem9_instrument_reading_3" value="{{ $rawData->chem9_instrument_reading_3 }}" required autofocus autocomplete="chem9_instrument_reading_3">
-            </div>
+                <div class="mb-3">
+                    <label for="chem9_instrument_reading_3">Instrument Reading 3</label>
+                    <input id="chem9_instrument_reading_3" type="number" name="chem9_instrument_reading_3" value="{{ $chem9s->chem9_instrument_reading_3 }}" required autofocus autocomplete="chem9_instrument_reading_3">
+                </div>
 
-            <div class="mb-3">
-                <label for="chem9_average_instrument_reading">Average Instrument Reading</label>
-                <input id="chem9_average_instrument_reading" type="text" name="chem9_average_instrument_reading" value="{{ $rawData->chem9_average_instrument_reading }}" required autofocus autocomplete="chem9_average_instrument_reading" readonly>
-            </div>
+                <div class="mb-3">
+                    <label for="chem9_average_instrument_reading">Average Instrument Reading</label>
+                    <input id="chem9_average_instrument_reading" type="text" name="chem9_average_instrument_reading" value="{{ $chem9s->chem9_average_instrument_reading }}" required autofocus autocomplete="chem9_average_instrument_reading" readonly>
+                </div>
 
-            <div class="mb-3">
-                <label for="chem9_final_result">Final Result</label>
-                <input id="chem9_final_result" type="number" name="chem9_final_result" value="{{ $rawData->chem9_final_result }}" required autofocus autocomplete="chem9_final_result">
-            </div>
+                <div class="mb-3">
+                    <label for="chem9_final_result">Final Result</label>
+                    <input id="chem9_final_result" type="number" name="chem9_final_result" value="{{ $chem9s->chem9_final_result }}" required autofocus autocomplete="chem9_final_result">
+                </div>
 
-            <div class="mb-3">
-                <label for="chem9_final_result_remarks">Final Result Remarks</label>
-                <input id="chem9_final_result_remarks" type="text" name="chem9_final_result_remarks" value="{{ $rawData->chem9_final_result_remarks }}" required autofocus autocomplete="chem9_final_result_remarks" readonly>
-            </div>
+                <div class="mb-3">
+                    <label for="chem9_final_result_remarks">Final Result Remarks</label>
+                    <input id="chem9_final_result_remarks" type="text" name="chem9_final_result_remarks" value="{{ $chem9s->chem9_final_result_remarks }}" required autofocus autocomplete="chem9_final_result_remarks" readonly>
+                </div>
+            @endforeach
         </div>
     </div>
