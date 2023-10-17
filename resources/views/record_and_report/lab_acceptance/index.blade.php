ae5f769ca44d26d20b335524d397dc6275c84992
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-10-17 16:00:56 +0800

    design table lab-acceptance

--- resources/views/record_and_report/lab_acceptance/index.blade.php
+++ resources/views/record_and_report/lab_acceptance/index.blade.php
@@ -3,14 +3,15 @@
 @section('content')
 
     <div class="mb-3 bg-white card">
-        <div class="flex justify-end gap-2">
-            <div class="justify-end lg:flex">
+        <div class="flex justify-between gap-2">
+            <div class="justify-start lg:flex">
                 @include('components.datefilter')
             </div>
             <div class="justify-end lg:flex">
                 @include('components.search')
             </div>
         </div>
+
         <div class="relative overflow-x-auto sm:rounded-lg">
             <table class="w-full text-sm text-left text-gray-500">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50">
