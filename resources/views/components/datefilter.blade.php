20832b40d462387ddf7c78a7a839fc0bfe6f5a3a
Jhunriz <93062821+Jhunriz@users.noreply.github.com>
2023-10-27 16:39:02 +0800

    date_fix_selection

--- resources/views/components/datefilter.blade.php
+++ resources/views/components/datefilter.blade.php
@@ -1,18 +1,18 @@
 <form>
     <div class="flex items-center gap-2">
         <div>
-            <input type="date" id="start_date" name="start_date" placeholder="Start Date" @if (!empty($start_date))
-                value="{{ $start_date }}" @endif required>
+            <input type="date" id="start_date" name="start_date" placeholder="Start Date"
+                @if (!empty($start_date)) value="{{ $start_date }}" @endif required>
         </div>
         <h1>To</h1>
         <div>
-            <input type="date" id="end_date" name="end_date" placeholder="End Date" @if (!empty($end_date))
-                value="{{ $end_date }}" @endif required>
+            <input type="date" id="end_date" name="end_date" placeholder="End Date"
+                @if (!empty($end_date)) value="{{ $end_date }}" @endif required>
         </div>
         <button class="btn btn-primary" type="submit">
             Apply
         </button>
-        <div class="flex items-center hidden p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert"
+        <div class="items-center hidden p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert"
             id="alert">
             <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor" viewBox="0 0 20 20">
@@ -29,23 +29,18 @@
 </form>
 
 <script>
-    function checkEndDate() {
+    document.querySelector(".btn-primary").addEventListener("click", function(event) {
         var startValue = document.getElementById("start_date").value;
         var endValue = document.getElementById("end_date").value;
-        var currentDate = new Date().toISOString().split('T')[0];
+        var alert = document.getElementById("alert");
+        var startDate = new Date(startValue);
+        var endDate = new Date(endValue);
 
-        if (startValue.trim() === '' || endValue.trim() === '') {
-            // At least one date is empty
-            document.getElementById("alert").classList.remove("hidden");
-            event.preventDefault(); // Prevent form submission
-        } else if (endValue !== currentDate) {
-            document.getElementById("alert").classList.remove("hidden");
-            event.preventDefault(); // Prevent form submission
+        if (!startValue || !endValue || startDate > endDate) {
+            alert.classList.remove("hidden");
+            event.preventDefault(); // Prevent form submission on validation failure
         } else {
-            document.getElementById("alert").classList.add("hidden");
+            alert.classList.add("hidden");
         }
-    }
-
-    // Add an event listener to the "Apply" button to run the checkEndDate function when clicked
-    document.querySelector(".btn-primary").addEventListener("click", checkEndDate);
+    });
 </script>
