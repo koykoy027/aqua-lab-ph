30a914246b1f5a481431f85209698721cfa91566
koykoy027 <villanuevajoshua27@gmail.com>
2024-02-21 16:34:00 +0800

    show remarks in schedule and sample information

--- app/Http/Controllers/ScheduleAndSampleInformationController.php
+++ app/Http/Controllers/ScheduleAndSampleInformationController.php
@@ -9,26 +9,28 @@ use App\Models\AnalysisRequest;
 class ScheduleAndSampleInformationController extends Controller
 {
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $query = $request->input('search');
 
         $clients = Client::where('account_number', 'LIKE', "%$query%")
-        ->orWhere('account_name', 'LIKE', "%$query%")
-        ->orWhere('email', 'LIKE', "%$query%")
-        ->orWhere('municipality_or_city', 'LIKE', "%$query%")
-        ->orderByDesc('updated_at')
-        ->paginate(10);
+            ->orWhere('account_name', 'LIKE', "%$query%")
+            ->orWhere('email', 'LIKE', "%$query%")
+            ->orWhere('municipality_or_city', 'LIKE', "%$query%")
+            ->orderByDesc('updated_at')
+            ->paginate(10);
 
         return view('service.schedule_and_sample_information.client_table', compact('clients', 'query'));
     }
 
 
-    public function show($account_number){
+    public function show($account_number)
+    {
         $clients = Client::find($account_number);
         $analysisRequest = AnalysisRequest::query()
-        ->where('account_number', $account_number)
+            ->where('account_number', $account_number)
 
-        ->paginate(10);
+            ->paginate(10);
 
         return view('service.schedule_and_sample_information.profile_table', compact(
             'analysisRequest',
--- resources/views/service/schedule_and_sample_information/profile_table.blade.php
+++ resources/views/service/schedule_and_sample_information/profile_table.blade.php
@@ -2,134 +2,133 @@
 @section('title', 'Samples')
 @section('content')
 
-    <div class="mb-3 bg-white card">
-        <div class="flex justify-between items-center gap-2">
-            <div class="ml-1 text-sm font-medium text-gray-500 md:ml-2s">
-                <h1 class="uppercase">@yield('title')</h1>
-                <p class="text-xs">List of all Samples by {{ $clients->account_name }}</p>
-            </div>
-        </div>
-
-        <div class="relative overflow-x-auto sm:rounded-lg">
-            <table class="w-full text-sm text-left text-gray-500">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Sample ID
-                        </th>
-                        {{-- <th scope="col" class="px-6 py-3">
-                            Account Name
-                        </th> --}}
-                        <th scope="col" class="px-6 py-3">
-                            Remarks
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Collected By
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Source of Water Sample
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Type of water
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Water Purpose
-                        </th>
-
-                        <th scope="col" class="px-6 py-3">
-                            Test Parameters
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Time
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Date
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Next Test
-                        </th>
-                        {{-- <th scope="col" class="px-6 py-3">
-                            <span class="sr-only">Actions</span>
-                        </th> --}}
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($analysisRequest as $data)
-                        <tr class="bg-white border-b">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                {{ $data->analysis_id_ }}
-                            </th>
-
-                            <td class="px-6 py-4">
-
-                                <span
-                                    class="mr-2 rounded px-2.5 py-0.5 text-xs font-medium text-center
-                                    @if ($data->remarks === 'Pending') text-orange-800 bg-orange-100 @endif
-                                    @if ($data->remarks === 'Conditionally Accepted') text-violet-800 bg-violet-100 @endif
-                                    @if ($data->remarks === 'Accepted') text-green-800 bg-green-100 @endif
-                                    @if ($data->remarks === 'Rejected') text-red-800 bg-red-100 @endif
-                                    @if ($data->remarks === 'Approve') text-blue-800 bg-blue-100 @endif
-                                    @if ($data->remarks === 'Disapprove') text-yellow-800 bg-yellow-100 @endif
-                                    @if ($data->remarks === 'Testing on-going') text-slate-800 bg-slate-100 @endif
-                                    @if ($data->remarks === 'For approval') text-pink-800 bg-pink-100 @endif
-                                    @if ($data->remarks === 'For releasing') text-cyan-800 bg-cyan-100 @endif
-
-                                ">
-                                    {{ $data->remarks }}
-                                </span>
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $data->collector_name }}
-                            </td>
-                            <td class="px-6 py-4">
-                                @if ($data->source_of_water_sample == 'Others')
-                                    {{ $data->source_of_water_sample_others }}
-                                @else
-                                    {{ $data->source_of_water_sample }}
-                                @endif
-                            </td>
-                            <td class="px-6 py-4">
-                                @if ($data->type_of_water == 'Others')
-                                    {{ $data->type_of_water_others }}
-                                @else
-                                    {{ $data->type_of_water }}
-                                @endif
-                            </td>
-                            <td class="px-6 py-4">
-                                @if ($data->water_purpose == 'Others')
-                                    {{ $data->water_purpose_others }}
-                                @else
-                                    {{ $data->water_purpose }}
-                                @endif
-
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $data->test_parameters_type }}
-                            </td>
-
-                            <td class="px-6 py-4">
-                                {{ \Carbon\Carbon::parse($data->time_collected)->format('g:i A') }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ \Carbon\Carbon::parse($data->date_collected)->format('m/d/Y') }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ \Carbon\Carbon::parse($data->date_next_schedule)->format('m/d/Y') }}
-                            </td>
-
-                            <td class="px-6 py-4 text-right">
-                                {{-- <a href="{{ route('laboratory.lab-acceptance.create', ['analysis_id' => $data->analysis_id]) }}"
-                                    class="font-medium text-blue-600 hover:underline">Lab Acceptance Form</a> --}}
-                            </td>
-                        </tr>
-                    @endforeach
-
-                </tbody>
-            </table>
-        </div>
-        <div class="my-5">
-            {{ $analysisRequest->links() }}
+<div class="mb-3 bg-white card">
+    <div class="flex items-center justify-between gap-2">
+        <div class="ml-1 text-sm font-medium text-gray-500 md:ml-2s">
+            <h1 class="uppercase">@yield('title')</h1>
+            <p class="text-xs">List of all Samples by {{ $clients->account_name }}</p>
         </div>
     </div>
+
+    <div class="relative overflow-x-auto sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Sample ID
+                    </th>
+                    {{-- <th scope="col" class="px-6 py-3">
+                        Account Name
+                    </th> --}}
+                    <th scope="col" class="px-6 py-3">
+                        Remarks
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Collected By
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Source of Water Sample
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Type of water
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Water Purpose
+                    </th>
+
+                    <th scope="col" class="px-6 py-3">
+                        Test Parameters
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Time
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Date
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Next Test
+                    </th>
+                    {{-- <th scope="col" class="px-6 py-3">
+                        <span class="sr-only">Actions</span>
+                    </th> --}}
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($analysisRequest as $data)
+                <tr class="bg-white border-b">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {{ $data->analysis_id_ }}
+                    </th>
+
+                    <td class="px-6 py-4">
+
+                        <span class="mr-2 rounded px-2.5 py-0.5 text-xs font-medium text-center
+                            @if ($data->labAcceptance->remarks == 'Pending') text-orange-800 bg-orange-100 @endif
+                            @if ($data->labAcceptance->remarks == 'Conditionally Accepted') text-violet-800 bg-violet-100 @endif
+                            @if ($data->labAcceptance->remarks == 'Accepted') text-green-800 bg-green-100 @endif
+                            @if ($data->labAcceptance->remarks == 'Rejected') text-red-800 bg-red-100 @endif
+                            @if ($data->labAcceptance->remarks == 'Approve') text-blue-800 bg-blue-100 @endif
+                            @if ($data->labAcceptance->remarks == 'Disapprove') text-yellow-800 bg-yellow-100 @endif
+                            @if ($data->labAcceptance->remarks == 'Testing on-going') text-slate-800 bg-slate-100 @endif
+                            @if ($data->labAcceptance->remarks == 'For approval') text-pink-800 bg-pink-100 @endif
+                            @if ($data->labAcceptance->remarks == 'For releasing') text-cyan-800 bg-cyan-100 @endif
+                        ">
+                            {{ $data->labAcceptance->remarks }}
+                        </span>
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $data->collector_name }}
+                    </td>
+                    <td class="px-6 py-4">
+                        @if ($data->source_of_water_sample == 'Others')
+                        {{ $data->source_of_water_sample_others }}
+                        @else
+                        {{ $data->source_of_water_sample }}
+                        @endif
+                    </td>
+                    <td class="px-6 py-4">
+                        @if ($data->type_of_water == 'Others')
+                        {{ $data->type_of_water_others }}
+                        @else
+                        {{ $data->type_of_water }}
+                        @endif
+                    </td>
+                    <td class="px-6 py-4">
+                        @if ($data->water_purpose == 'Others')
+                        {{ $data->water_purpose_others }}
+                        @else
+                        {{ $data->water_purpose }}
+                        @endif
+
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $data->test_parameters_type }}
+                    </td>
+
+                    <td class="px-6 py-4">
+                        {{ \Carbon\Carbon::parse($data->time_collected)->format('g:i A') }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ \Carbon\Carbon::parse($data->date_collected)->format('m/d/Y') }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ \Carbon\Carbon::parse($data->date_next_schedule)->format('m/d/Y') }}
+                    </td>
+
+                    <td class="px-6 py-4 text-right">
+                        {{-- <a
+                            href="{{ route('laboratory.lab-acceptance.create', ['analysis_id' => $data->analysis_id]) }}"
+                            class="font-medium text-blue-600 hover:underline">Lab Acceptance Form</a> --}}
+                    </td>
+                </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+    <div class="my-5">
+        {{ $analysisRequest->links() }}
+    </div>
+</div>
 
 @endsection
