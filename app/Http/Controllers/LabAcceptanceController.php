3201e534cf4f4774606108334c6c9ee17a6f215d
koykoy027 <villanuevajoshua27@gmail.com>
2024-02-26 12:15:51 +0800

    error fixed

--- app/Http/Controllers/LabAcceptanceController.php
+++ app/Http/Controllers/LabAcceptanceController.php
@@ -113,7 +113,7 @@ class LabAcceptanceController extends Controller
             ->paginate(10);
 
         return view('laboratory.lab_work_order.index', compact(
-            'analysisRequest', 
+            'analysisRequest',
             'query',
             'start_date',
             'end_date',
@@ -140,47 +140,50 @@ class LabAcceptanceController extends Controller
             'remarks' => 'required',
         ]);
 
-        try{
-            DB::beginTransaction();
+        DB::beginTransaction();
+
+        try {
+
+
             $currentDate = now(); // Get the current date
             // Get the month and date components
             $month = $currentDate->format('n') - 1; // Convert to 0-based index
             $date = $currentDate->format('d');
-    
+
             // Convert month to your desired pattern
             $monthPattern = chr(65 + $month); // 'A' corresponds to January, 'B' to February, and so on
-    
+
             $formattedDate = str_pad($date, 2, '0', STR_PAD_LEFT); // Format the date to be 2 digits (e.g., 01, 02, ..., 31)
             $findLabAcceptance = LabAcceptance::find($analysis_id);
             $findAnalysisRequest = AnalysisRequest::find($analysis_id);
-    
+
             // start of format
             $plainFormat = $monthPattern . $formattedDate;
-    
+
             $totalMicroSampleToday = LabAcceptance::whereDate('created_at', $currentDate->toDateString())
                 ->whereHas('analysisRequest', function ($queryBuilder) use ($findAnalysisRequest) {
                     $queryBuilder->where('test_parameters', 'micro'); // find if micro or pychem
                 })
                 ->count();
             $totalPyChemSampleToday = LabAcceptance::whereDate('created_at', $currentDate->toDateString())
-            ->whereHas('analysisRequest', function ($queryBuilder) use ($findAnalysisRequest) {
-                $queryBuilder->where('test_parameters', '!=', 'micro'); // find if micro or pychem
-            })
-            ->count();
+                ->whereHas('analysisRequest', function ($queryBuilder) use ($findAnalysisRequest) {
+                    $queryBuilder->where('test_parameters', '!=', 'micro'); // find if micro or pychem
+                })
+                ->count();
             $totalMicroSampleTodaySampleFormat = $plainFormat . (str_pad($totalMicroSampleToday, 2, '0', STR_PAD_LEFT));
             $totalPyChemSampleTodaySampleFormat = $plainFormat . (str_pad($totalPyChemSampleToday, 2, '0', STR_PAD_LEFT)) . 'PC';
             $finalFormat = $findAnalysisRequest->test_parameters == 'micro' ? $totalMicroSampleTodaySampleFormat : $totalPyChemSampleTodaySampleFormat;
             // end of format
-    
-    
-    
-            
-    
+
+
+
+
+
             if (!$findLabAcceptance->sample_id) { // store new sample_id
                 $findLabAcceptance->update([
                     'sample_id' => $finalFormat,
                 ]);
-    
+
                 // store sample conditions [multiple checkbox]
                 $selectedSampleCondition = $request->input('sample_condition', []);
                 foreach ($selectedSampleCondition as $data) {
@@ -190,8 +193,9 @@ class LabAcceptanceController extends Controller
                     ]);
                 }
             }
+
             $findLabAcceptance->update([ // update all fields
-                'analysis_id' => $request->analysis_id,
+                // 'analysis_id' => $request->analysis_id,
                 'evaluated_by' => $request->evaluated_by,
                 'date_evaluated' => $request->date_evaluated,
                 'time_evaluated' => $request->time_evaluated,
@@ -199,7 +203,7 @@ class LabAcceptanceController extends Controller
                 'remarks' => $request->remarks,
                 'if_remarks_are_rejected' => $request->if_remarks_are_rejected,
             ]);
-    
+
             $pendingFromLabAcceptance = LabAcceptance::query()
                 ->whereHas('analysisRequest', function ($queryBuilder) use ($findAnalysisRequest) {
                     if ($findAnalysisRequest->test_parameters == 'micro') {
@@ -211,7 +215,9 @@ class LabAcceptanceController extends Controller
                 })
                 ->where('remarks', 'Pending')
                 ->first();
-    
+            DB::commit();
+
+
             if ($pendingFromLabAcceptance) {
                 return redirect()
                     ->route('laboratory.lab-acceptance.create', $pendingFromLabAcceptance->analysis_id)
@@ -219,7 +225,7 @@ class LabAcceptanceController extends Controller
                         'message' => 'Lab acceptance has been created successfully! Redirect to next Pending acceptance'
                     ]);
             } else {
-    
+
                 $routeName = ($findAnalysisRequest->test_parameters == 'micro' ? 'service.lab-result-status.micro' : 'service.lab-result-status.pychem');
                 return redirect()
                     ->route($routeName)
@@ -227,11 +233,9 @@ class LabAcceptanceController extends Controller
                         'message' => 'Lab acceptance has been created successfully! No more Pending acceptance'
                     ]);
             }
-            DB::commit();
-        }catch(\Exception $error){
+        } catch (\Exception $error) {
             DB::rollBack();
             return redirect()->back('error', 'Ooops... Something went wrong. Please try again.');
         }
-        
     }
 }
