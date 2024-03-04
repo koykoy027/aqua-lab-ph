fc472eb196f7b1d33e1f67e53e0b98ecccaf8474
koykoy027 <villanuevajoshua27@gmail.com>
2024-03-04 08:52:57 +0800

    clients and analysis request nullable

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::factory(10)->create();
+        // Client::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
