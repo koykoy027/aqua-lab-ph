f8fad9b74665fcbb8cd7047c0fafd3b65c39d21d
koykoy027 <villanuevajoshua27@gmail.com>
2024-03-12 21:45:52 +0800

    test runners

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,20 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="png" href="{{ asset('image/branding.png') }}">
-    <title>{{ config('app.name') }}</title>
+    <title>test {{ config('app.name') }}</title>
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
+<body class="font-sans antialiased text-gray-900">
     @include('layouts.tailwindcss')
-    <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+    <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
         <div>
             <a href="{{ url('/') }}">
-                <img src="{{ asset('image/branding.png') }}" alt="AQUALAB PH" class="h-20 fill-current text-gray-500">
+                <img src="{{ asset('image/branding.png') }}" alt="AQUALAB PH" class="h-20 text-gray-500 fill-current">
                 {{-- AQUA LAB PH --}}
             </a>
         </div>
 
-        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+        <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
             @yield('content')
         </div>
     </div>
